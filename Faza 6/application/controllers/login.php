4be299751619e435c8889e461dff6a3c82852ed3
Nemanja <nemanjabogdanovic@outlook.com>
2018-05-26 02:32:49 +0200

    login_v0.1

--- Faza 6/application/controllers/login.php
+++ Faza 6/application/controllers/login.php
@@ -18,41 +18,5 @@
 			$this->load->view('templates/footer');
 		}
 		
-		//login
-		/*
-		public function login(){
-			$data['title'] = 'Sign In';
-			$this->form_validation->set_rules('korisnickoIme', 'Korisnicko_Ime', 'required');
-			$this->form_validation->set_rules('sifra', 'Sifra', 'required');
-			if($this->form_validation->run() === FALSE){
-				$this->load->view('templates/header');
-				$this->load->view('login/login', $data);
-				$this->load->view('templates/footer');
-			} else {
-				
-				// Get username
-				$username = $this->input->post('korisnickoIme');
-				// Get and encrypt the password
-				$password = md5($this->input->post('sifra'));
-				// Login user
-				$user_id = $this->user_model->login($username, $password);
-				if($user_id){
-					// Create session
-					$user_data = array(
-						'idKorisnika' => $user_id,
-						'korisnickoIme' => $username,
-						'logged_in' => true
-					);
-					$this->session->set_userdata($user_data);
-					// Set message
-					$this->session->set_flashdata('user_loggedin', 'You are now logged in');
-					redirect('posts');
-				} else {
-					// Set message
-					$this->session->set_flashdata('login_failed', 'Login is invalid');
-					redirect('users/login');
-				}		
-			}
-		}
-		*/
+		
 	}
