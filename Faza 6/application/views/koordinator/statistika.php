93887f59d0460adf25dadd3e7dd506d324130f2f
Nemanja <nemanjabogdanovic@outlook.com>
2018-05-29 12:40:10 +0200

    bootstrap_fixes

--- Faza 6/application/views/koordinator/statistika.php
+++ Faza 6/application/views/koordinator/statistika.php
@@ -2,22 +2,20 @@
 	autor: Nemanja Bogdanovic, 2012/0533
 	@version: 1.0
 -->
-<div class="css-container">
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 black-bg left-container">
-			<div class="tm-left-inner-container">
-				<ul class="nav nav-stacked css-nav">
-					<li><a href="<?php echo base_url(); ?>koordinator/home">Početna</a></li>
-					<li><a href="<?php echo base_url(); ?>koordinator/predmeti">Predmeti</a></li>
-					<li><a href="<?php echo base_url(); ?>koordinator/raspored">Raspored časova</a></li>
-					<li><a href="<?php echo base_url(); ?>koordinator/uredjivanje">Uređivanje naloga</a></li>
-					<li><a href="<?php echo base_url(); ?>koordinator/statistika">Statistika</a></li>
-				</ul>
-			</div>
-		</div>
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 white-bg right-container">	
-			<div class="tm-right-inner-container">
-				<h1 class="css-header">Statistika</h1>		
-				<p>Koordinator</p>
-			</div>	
-		</div>
-</div>	
+<div class="col col-lg-2 col-md-3 col-sm-3 col-xs-12 left-container">
+	<div class="tm-left-inner-container">
+		<ul class="nav nav-stacked css-nav">
+			<li><a href="<?php echo base_url(); ?>koordinator/home">Početna</a></li>
+			<li><a href="<?php echo base_url(); ?>koordinator/predmeti">Predmeti</a></li>
+			<li><a href="<?php echo base_url(); ?>koordinator/raspored">Raspored časova</a></li>
+			<li><a href="<?php echo base_url(); ?>koordinator/uredjivanje">Uređivanje naloga</a></li>
+			<li><a href="<?php echo base_url(); ?>koordinator/statistika">Statistika</a></li>
+		</ul>
+	</div>
+</div>
+<div class="col col-lg-8 col-md-7 col-sm-7 col-xs-12 right-container">		
+	<div class="tm-right-inner-container">
+		<h1 class="css-header">Statistika</h1>		
+		<p>Koordinator</p>
+	</div>	
+</div>
--- Faza 6/application/views/nastavnik/home.php
+++ Faza 6/application/views/nastavnik/home.php
@@ -2,23 +2,21 @@
 	autor: Nemanja Bogdanovic, 2012/0533
 	@version: 1.0
 -->
-<div class="css-container">
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 black-bg left-container">
-			<div class="tm-left-inner-container">
-				<ul class="nav nav-stacked css-nav">
-					<li><a href="<?php echo base_url(); ?>nastavnik/home">Početna</a></li>
-					<li><a href="<?php echo base_url(); ?>nastavnik/kalendar">Kalendar</a></li>
-					<li><a href="<?php echo base_url(); ?>nastavnik/ucenici">Učenici</a></li>
-					<li><a href="<?php echo base_url(); ?>nastavnik/upis">Upis časa</a></li>
-					<li><a href="<?php echo base_url(); ?>nastavnik/izostanci">Izostanci</a></li>
-					<li><a href="<?php echo base_url(); ?>nastavnik/ocene">Ocene</a></li>
-				</ul>
-			</div>
-		</div>
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 white-bg right-container">	
-			<div class="tm-right-inner-container">
-				<h1 class="css-header">Početna</h1>		
-				<p>Nastavnik</p>
-			</div>	
-		</div>
-</div>	
+<div class="col col-lg-2 col-md-3 col-sm-3 col-xs-12 left-container">
+	<div class="tm-left-inner-container">
+		<ul class="nav nav-stacked css-nav">
+			<li><a href="<?php echo base_url(); ?>nastavnik/home">Početna</a></li>
+			<li><a href="<?php echo base_url(); ?>nastavnik/kalendar">Kalendar</a></li>
+			<li><a href="<?php echo base_url(); ?>nastavnik/ucenici">Učenici</a></li>
+			<li><a href="<?php echo base_url(); ?>nastavnik/upis">Upis časa</a></li>
+			<li><a href="<?php echo base_url(); ?>nastavnik/izostanci">Izostanci</a></li>
+			<li><a href="<?php echo base_url(); ?>nastavnik/ocene">Ocene</a></li>
+		</ul>
+	</div>
+</div>
+<div class="col col-lg-8 col-md-7 col-sm-7 col-xs-12 right-container">		
+	<div class="tm-right-inner-container">
+		<h1 class="css-header">Početna</h1>		
+		<p>Nastavnik</p>
+	</div>	
+</div>
