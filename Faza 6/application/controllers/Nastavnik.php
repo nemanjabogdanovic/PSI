f8fea2c70cd80015fa09fef9fc8fb123bc74b016
Milosmisa8 <38259211+Milosmisa8@users.noreply.github.com>
2018-06-01 10:50:32 +0200

    Revert "Merge branch 'master' of https://github.com/nemanjabogdanovic/PSI"
    This reverts commit 26ee5a6c1a30c58633399fb7bdeac752cb51d34d, reversing
    changes made to 5047919e8d9b7867edfa8eb46e2bacf07858a140.

--- Faza 6/application/controllers/Nastavnik.php
+++ Faza 6/application/controllers/Nastavnik.php
@@ -20,20 +20,15 @@
 			if(!file_exists(APPPATH.'views/nastavnik/'.$page.'.php')){
 				show_404();
 			}
-
+			
 			if(!$this->session->userdata('user_level') === 'nastavnik'){
 				redirect('users/login');
 			}
-
+			
 			$data['title'] = ucfirst($page);
-
+			
 			$this->load->view('templates/header');
 			$this->load->view('nastavnik/'.$page, $data);
 			$this->load->view('templates/footer');
-
-
 		}
-
-
-
 	}
